dcf78208c5081f31e554e8c23dc5884dac0d3e94
wiktorcie <wiktorcie@gmail.com>
2024-09-14 17:59:22 +0200

    disable email sending

--- config/settings/base.py
+++ config/settings/base.py
@@ -306,7 +306,7 @@ ACCOUNT_USERNAME_REQUIRED = False
 # https://docs.allauth.org/en/latest/account/configuration.html
 ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 # https://docs.allauth.org/en/latest/account/configuration.html
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_VERIFICATION = "none"
 # https://docs.allauth.org/en/latest/account/configuration.html
 ACCOUNT_ADAPTER = "kempingdabrowno.users.adapters.AccountAdapter"
 # https://docs.allauth.org/en/latest/account/forms.html
